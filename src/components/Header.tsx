d859bfc2273cd08748a3a351700e24dd6a40c618
guilherme.fittipaldi <guilherme.fittipaldi@fourcicle.com.br>
2022-10-27 10:49:30 -0300

    feat: adding header

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import "./header.css";
+import { TextField } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <span>OldNews</span>
+      </div>
+      <div className="search">
+        <TextField
+          className="searchInput"
+          placeholder="Pesquisar"
+          InputProps={{
+            disableUnderline: true,
+            endAdornment: <SearchIcon className="searchIcon" />,
+          }}
+        />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
