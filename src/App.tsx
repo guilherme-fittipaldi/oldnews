076fc3febdc50404d228d62075af252d386f1dd3
guilherme-fittipaldi <guilherme.fittipaldi6@gmail.com>
2022-10-29 13:17:24 -0300

    feat: changing font to GT Walsheim Pro

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import React from "react";
-
 import "./App.css";
 import Header from "./components/Header/Header";
 import Articles from "./components/Articles/Articles";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      "GT Walsheim Pro",
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+    ].join(","),
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-weight: bold;
+        }
+      `,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header />
       <Articles />
-    </>
+    </ThemeProvider>
   );
 }
 
