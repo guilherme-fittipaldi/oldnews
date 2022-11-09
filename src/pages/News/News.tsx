159dc4bb13fdbd4d8d2954651be74634a00408d2
Henrique Rinaldi <henriquergr@hotmail.com>
2022-11-08 22:10:35 -0300

    wip: News page

--- src/pages/News/News.tsx
+++ src/pages/News/News.tsx
@@ -1,6 +1,36 @@
+import { Typography, Card, CardMedia } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2";
+
 function News() {
   return (
-    <h1>Notícia</h1>
+    <Grid2
+      container
+      display="flex"
+      direction="column"
+      alignItems="center"
+    >
+      <Grid2>
+        <Typography variant="h4" component="h1">Notícia</Typography>
+      </Grid2>
+      <Grid2>
+        <Card
+          sx={{
+            display: "flex",
+            alignItems: "start",
+            boxShadow: 0,
+            backgroundColor: "transparent",
+          }}
+        >
+          <CardMedia
+            component="img"
+            sx={{ width: "900px" }}
+            height="500"
+            image="https://cdn.britannica.com/04/5104-004-51F5F120/Flag-Antigua-and-Barbuda.jpg?w=400&h=200&c=crop"
+            alt="Antigua and Barbuda achieved independence from the United Kingdom"
+          />
+        </Card>
+      </Grid2>
+    </Grid2>
   );
 }
 
